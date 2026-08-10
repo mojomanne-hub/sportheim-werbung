66cbc7f9b8f246b44e499f4b9b270de29be01534
Manne <madetofind@web.de>
2026-08-11 01:51:56 +0200

    WFV-Pokal-Route hinzugefügt

--- app/pokal/page.tsx
+++ app/pokal/page.tsx
@@ -10,9 +10,9 @@ export default function PokalPage() {
         src="https://www.fussball.de/spieltag/1-runde-bezirk-oberschwaben-bezirkspokal-herren-saison2627-wuerttemberg/-/spieldatum/2026-08-11/staffel/030TM3AU50000001VS5489BUVSEBP30S-R#!/"
         style={{
           width: '100%',
-          height: '200%',
+          height: '150%',
           border: 'none',
-          marginTop: '-600px',
+          marginTop: '-900px',
           marginLeft: '0px',
         }}
       />
