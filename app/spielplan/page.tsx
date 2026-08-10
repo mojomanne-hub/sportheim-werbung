5e6b2080356477c129ffb9c124ba7924a22a3ed4
Manne <madetofind@web.de>
2026-08-10 23:52:02 +0200

    Spielplan-Ausschnitt angepasst

--- app/spielplan/page.tsx
+++ app/spielplan/page.tsx
@@ -6,16 +6,21 @@ export default function SpielplanPage() {
       overflow: 'hidden',
       background: '#fff',
     }}>
-      <iframe
-        src="https://sportheim-werbung.vercel.app/widget/df2f6bf0-ec97-4a69-9a6d-9efee44fac96"
-        style={{
-          width: '70%',
-          height: '70%',
-          border: 'none',
-          marginLeft: '-10%',
-          marginTop: '-10%',
-        }}
-      />
+      <div style={{
+        width: '200%',
+        height: '200%',
+        marginLeft: '-45%',
+        marginTop: '-15%',
+      }}>
+        <iframe
+          src="https://sportheim-werbung.vercel.app/widget/DEINE_ID"
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none',
+          }}
+        />
+      </div>
     </div>
   )
 }
