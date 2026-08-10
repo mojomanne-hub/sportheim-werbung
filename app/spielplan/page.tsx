fc756b4c07cac2161f673ca67f1cd6f00496d626
Manne <madetofind@web.de>
2026-08-11 01:40:36 +0200

    Liga1-Ansicht verkleinert, mehr Spiele anzeigen

--- app/spielplan/page.tsx
+++ app/spielplan/page.tsx
@@ -12,8 +12,8 @@ export default function SpielplanPage() {
     width: '100%',
     height: '200%',
     border: 'none',
-    marginTop: '-550px',    // ← Statt -200px, weiter runter
-    marginLeft: '-70px',   // ← Neu, nach rechts verschieben
+    marginTop: '-650px',    // ← Statt -200px, weiter runter
+    marginLeft: '-40px',   // ← Neu, nach rechts verschieben
   }}
 />
     </div>
