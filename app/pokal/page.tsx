4043a5cebe418ab675152ff21b262da20e72b621
Manne <madetofind@web.de>
2026-08-11 02:11:41 +0200

    Liga1-Ansicht verkleinert, mehr Spiele anzeigen

--- app/pokal/page.tsx
+++ app/pokal/page.tsx
@@ -5,19 +5,15 @@ export default function PokalPage() {
       height: '100vh',
       overflow: 'hidden',
       background: '#fff',
-      position: 'relative',
     }}>
       <iframe
-     src="https://www.fussball.de/spieltag/1-runde-bezirk-oberschwaben-bezirkspokal-herren-saison2627-wuerttemberg/-/spieldatum/2026-08-11/staffel/030TM3AU50000001VS5489BUVSEBP30S-R#!/"
-
+        src="https://www.fussball.de/spieltag/1-runde-bezirk-oberschwaben-bezirkspokal-herren-saison2627-wuerttemberg/-/spieldatum/2026-08-11/staffel/030TM3AU50000001VS5489BUVSEBP30S-R#!/"
         style={{
-          width: '100%',
-          height: '100%',
+          width: '120%',
+          height: '200%',
           border: 'none',
-          display: 'block',
-          position: 'absolute',
-          top: 0,
-          left: 0,
+          marginTop: '-950px',
+          marginLeft: '0px',
         }}
       />
     </div>
