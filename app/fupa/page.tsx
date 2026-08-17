962c801abb7927be69f3c85e3de73161ab151e42
Manne <madetofind@web.de>
2026-08-17 22:15:08 +0200

    FUPA-Route hinzugefügt

---
+++ app/fupa/page.tsx
@@ -0,0 +1,19 @@
+export default function FupaPage() {
+  return (
+    <div style={{
+      width: '100vw',
+      height: '100vh',
+      overflow: 'hidden',
+      background: '#fff',
+    }}>
+      <iframe
+        src="https://www.fupa.net/league/bezirksliga-oberschwaben/matches"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+        }}
+      />
+    </div>
+  )
+}
