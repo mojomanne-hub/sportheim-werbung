c97d625bfe8da88a98816dfffeb8c3b8c8f18525
Manne <madetofind@web.de>
2026-08-17 22:37:25 +0200

    Loading-Screen entfernt

--- app/spielplan/page.tsx
+++ app/spielplan/page.tsx
@@ -6,28 +6,16 @@ export default function SpielplanPage() {
       overflow: 'hidden',
       background: '#fff',
     }}>
-      <div style={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        fontSize: '20px',
-        color: '#666',
-        zIndex: 1,
-      }}>
-        Lädt Spielplan...
-      </div>
-      
       <iframe
-        src="https://www.fussball.de/spieltagsuebersicht/bezirksliga-bezirk-oberschwaben-bezirksliga-herren-saison2627-wuerttemberg/-/staffel/0319U1GHVS000006VS5489BTVSK8S3O6-G#!/"
-        style={{
-          width: '100%',
-          height: '200%',
-          border: 'none',
-          marginTop: '-600px',
-          marginLeft: '0px',
-        }}
-      />
+  src="https://www.fussball.de/spieltagsuebersicht/bezirksliga-bezirk-oberschwaben-bezirksliga-herren-saison2627-wuerttemberg/-/staffel/0319U1GHVS000006VS5489BTVSK8S3O6-G#!/"
+  style={{
+    width: '100%',
+    height: '200%',
+    border: 'none',
+    marginTop: '-600px',    // ← Statt -200px, weiter runter
+    marginLeft: '10px',   // ← Neu, nach rechts verschieben
+  }}
+/>
     </div>
   )
 }
